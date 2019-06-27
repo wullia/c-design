81a598d32214fe28a0ed6824435982c03d5198ed
wullia <45321982+wullia@users.noreply.github.com>
2019-06-27 13:30:16 +0800

    test
    aaaa

--- func.h
+++ func.h
@@ -10,7 +10,7 @@
 struct node{
 	int stuIdNum;
 	char name[20];
-    char gender[4];
+    char gender[6];
     float weight;
     int vital_capacity;
     int chin;
@@ -27,6 +27,12 @@ void print(Lnode );
 void AppendNode(Lnode &l);
 void DeleteNode(Lnode &l);
 void FindNode(Lnode &l);
-void change(Lnode &l)
+void change(Lnode &l);
+void change(Lnode &l);
+void Sort(Lnode l);
+void quickSort(Lnode l);
+void quick_sort(node * start, node * end);
+node * partion(node * start, node * end);
+void swap(node * a, node * b);
 #endif 
 
